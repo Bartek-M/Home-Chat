0696c466ba1afe7c0e9b5dd4069937d79b8a42bd
Bartosz Mroczkowski <bmroczkowski@icloud.com>
2022-12-30 21:09:20 +0100

    Login, signin, base, styles - frontend work

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import view
 
+# GLOBAL VARIABLES
+ADDR = "192.168.0.194"
+PORT = 5000
+
+# INITIALIZE FLASK
 app = Flask(__name__)
+app.secret_key = 'jkuQ/jM"?L5Vh]071iE{P9ziv?7xQUeeA8rFZ9*{' # Secret key for session
 app.register_blueprint(view, url_prefix="/")
 
+
+# not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("not_found.html", kwargs={"theme": True}), 404
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=ADDR, port=PORT, debug=True)
--- views.py
+++ views.py
@@ -1,19 +1,56 @@
-# Imports
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, session, flash
 
 view = Blueprint(__name__, "view") # Define views
+kwargs = {"theme": False}
 
-# Nothing
-@view.route("")
-def blank_page():
-    return redirect(url_for("views.login"))
 
-# Login Page
-@view.route("login")
-def login():
-    return "<h1>Login Page</h1>"
-
-# Home Page
-@view.route("home")
+# PAGES
+@view.route("/")
+@view.route("/home", methods=["POST", "GET"])
 def home():
-    return "<h1>Home Page</h1>"
+    if "user" not in session:
+        flash("You are not logged in!", "info")
+        return redirect(url_for("views.log_in"))
+
+    usr = session["user"]    
+    return render_template("index.html", text=f"Hello {usr}!", kwargs=kwargs) # Render home page
+
+
+@view.route("/login", methods=["POST", "GET"])
+def log_in():
+    if "user" in session:
+        flash("You are already logged in!", "info")
+        return redirect(url_for("views.home"))
+
+    if request.method == "POST":
+        user = request.form["usrname"]
+        session["user"] = user
+
+        flash("You have been logged in!", "info")
+        return redirect(url_for("views.home"))
+            
+    return render_template("login.html", kwargs=kwargs)
+
+
+@view.route("/signup", methods=["POST", "GET"])
+def sign_up():
+    if request.method == "POST":
+        if "user" in session:
+            session.pop("user", None)
+            
+        flash("You have created an account!", "info")
+        return redirect(url_for("views.log_in"))
+
+    return render_template("signup.html", kwargs=kwargs)
+
+
+@view.route("/logout")
+def log_out():
+    if "user" in session:
+        session.pop("user", None)
+        flash("You have been logged out!", "info")
+
+    return redirect(url_for("views.log_in"))
+
+
+# GENERAL FUNCTIONS
