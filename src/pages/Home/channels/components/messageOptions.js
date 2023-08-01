753021103e95606e8a0eae9fc9d51212a4ad663d
Bartosz Mroczkowski <bmroczkowski@icloud.com>
2023-08-01 21:27:33 +0200

    Displaying message options according to message author, channel owner/admin

--- src/pages/Home/channels/components/messageOptions.js
+++ src/pages/Home/channels/components/messageOptions.js
@@ -1,11 +1,22 @@
+import { useActive, useUser } from "../../../../context"
+
 export function MessageOptions({ message }) {
+    const [user,] = useUser()
+    const [active,] = useActive()
+    const channel = active.channel
+
+    if (!message || !channel) return null
+    if (![channel.owner, message.author].includes(user.id) && !(channel.users[user.id] && channel.users[user.id].admin)) return null
+
     return (
         <div className="message-options-wrapper container">
-            <button className="message-options-btn message-edit-btn center-container">
-                <svg width="12" height="12" fill="var(--FONT_RV_COLOR)" viewBox="0 0 16 16">
-                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                </svg>
-            </button>
+            {user.id === message.author &&
+                <button className="message-options-btn message-edit-btn center-container">
+                    <svg width="12" height="12" fill="var(--FONT_RV_COLOR)" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                    </svg>
+                </button>
+            }
             <button className="message-options-btn message-delete-btn center-container">
                 <svg width="16" height="16" fill="var(--RED_BUTTON)" viewBox="0 0 16 16">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
